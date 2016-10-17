109f272bdae42e9a7a2e12d6cdce78d83ae32f48
Crystal Qian <crystaljqian@gmail.com>
2016-10-17 10:50:50 -0400

    Probability added.

--- FaceNoise/NoiseGenerator.cs
+++ FaceNoise/NoiseGenerator.cs
@@ -6,10 +6,12 @@ namespace FaceNoise
     class NoiseGenerator
     {
         private Bitmap _bitmap;
+        private int _bitsModded;
         private Random _random;
         public NoiseGenerator(String fileName)
         {
             _bitmap = new Bitmap(fileName);
+            _bitsModded = 0;
             _random = new Random();
         }
 
@@ -27,33 +29,40 @@ namespace FaceNoise
         public Color NaiveNoise(Color pixel, double intensity)
         {
             // 0 will have no change.
-            var rChange = (int) (RandomColorValue() * intensity);
+            var rChange = (int)(RandomColorValue() * intensity);
             var rValue = Clamp(pixel.R + rChange);
 
-            var gChange = (int) (RandomColorValue() * intensity);
+            var gChange = (int)(RandomColorValue() * intensity);
             var gValue = Clamp(pixel.G + rChange);
 
-            var bChange = (int) (RandomColorValue() * intensity);
+            var bChange = (int)(RandomColorValue() * intensity);
             var bValue = Clamp(pixel.B + rChange);
 
             var color = Color.FromArgb(rValue, gValue, bValue);
+
+            _bitsModded += Math.Abs(rChange) + Math.Abs(gChange) + Math.Abs(bChange);
             return color;
         }
 
-        public void MakeNoise(double intensity)
+        public void MakeNoise(double intensity, double probability)
         {
             for (int i = 0; i < _bitmap.Height; i++)
             {
                 for (int j = 0; j < _bitmap.Width; j++)
                 {
-                    var color = NaiveNoise(_bitmap.GetPixel(j, i), intensity);
-                    _bitmap.SetPixel(j, i, color);
+                    var p = _random.NextDouble();
+                    if (p < probability)
+                    {
+                        var color = NaiveNoise(_bitmap.GetPixel(j, i), intensity);
+                        _bitmap.SetPixel(j, i, color);
+                    }
                 }
             }
         }
 
         public void Export(String fileName)
         {
+            Console.WriteLine("Bits modded: " + _bitsModded);
             _bitmap.Save(fileName);
         }
     }
--- FaceNoise/Program.cs
+++ FaceNoise/Program.cs
@@ -8,9 +8,10 @@ namespace FaceNoise
             var importFile = args[0];
             var exportFile = args[1];
             var intensity = Double.Parse(args[2]);
+            var probability = Double.Parse(args[3]);
 
             var noiseGenerator = new NoiseGenerator(importFile);
-            noiseGenerator.MakeNoise(intensity);
+            noiseGenerator.MakeNoise(intensity, probability);
             noiseGenerator.Export(exportFile);
         }
     }
